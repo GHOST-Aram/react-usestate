6ca3df74eb14fcacd1a70df43c89ad3dcc5a39d8
GHOST-Aram <owinoojala@gmail.com>
2023-04-27 10:59:03 +0300

    useEffect hook: Fetch data onSubmit

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -5,21 +5,32 @@ const DataFetching = () => {
     const [user, setuser] = useState({})
 
     const [id, setId] = useState(1)
+    const [idFromSubmit, setIdFromSubmit] = useState(1)
 
     useEffect(() =>{
         const fetchData = async() =>{
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${idFromSubmit}`)
             console.log(response.data)
             return response.data
         }
         console.log('Effect')
+    
         fetchData().then((data) =>{
             setuser(data)
         }).catch(error => console.log(error))
-    },[id])
+            
+        
+    },[idFromSubmit])
   return (
     <div>
+        <form action="" onSubmit={(e) =>{
+            e.preventDefault()
+            setIdFromSubmit(id)
+
+        }}>
         <input type="text" value={id} onChange={(e) =>setId(e.target.value)} />
+        <input type="submit" value="Submit" />
+        </form>
         <h1>{user.website}</h1>
         {/* {
             posts.map(user => <li key={user.email}>{user.name}</li>)
