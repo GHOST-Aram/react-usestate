09b1f71178f4a3c1fc2e9556e2c53915cb32ed96
GHOST-Aram <owinoojala@gmail.com>
2023-04-27 10:37:16 +0300

    feat: Fethc data of single user from an endpoint in useEffect

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,24 +2,28 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const DataFetching = () => {
-    const [posts, setPosts] = useState([])
+    const [user, setuser] = useState({})
 
-    const fetchData = async() =>{
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-        console.log(response.data)
-        return response.data
-    }
+    const [id, setId] = useState(1)
+
     useEffect(() =>{
+        const fetchData = async() =>{
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+            console.log(response.data)
+            return response.data
+        }
         console.log('Effect')
         fetchData().then((data) =>{
-            setPosts(data)
-        })
-    },[])
+            setuser(data)
+        }).catch(error => console.log(error))
+    },[id])
   return (
     <div>
-        {
+        <input type="text" value={id} onChange={(e) =>setId(e.target.value)} />
+        <h1>{user.website}</h1>
+        {/* {
             posts.map(user => <li key={user.email}>{user.name}</li>)
-        }
+        } */}
     </div>
   )
 }
