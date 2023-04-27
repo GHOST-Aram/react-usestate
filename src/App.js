ae924a3ea6cbfeabb34b6dd153bdd9b6c288374f
GHOST-Aram <owinoojala@gmail.com>
2023-04-27 09:49:35 +0300

    feat: useEffect with dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import StateHooks5 from './components/StateHooks5';
-import EffectHook1 from './components/EffectHook1';
+// import EffectHook1 from './components/EffectHook1';
+import IntervalHookCounter from './components/IntervalHookCounter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <EffectHook1 />
+      <IntervalHookCounter />
     </div>
   );
 }
---
+++ src/components/IntervalHookCounter.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const IntervalHookCounter = () => {
+    const [counter, setCounter] = useState(0)
+
+    
+    const tick = (counter) =>{
+        setCounter(counter + 1)
+    }
+    useEffect(()=>{
+        const interval = setInterval(()=> tick(counter), 1000)
+
+        return () => {clearInterval(interval)}
+    }, [counter])
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default IntervalHookCounter
